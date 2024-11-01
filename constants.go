3efac8168ff1020103dfd5c42ce1eeb70341e3fb
r. ávila <avila.dev@outlook.com>
2024-10-31 22:12:03 -0300

    feat: add new constants

--- constants.go
+++ constants.go
@@ -1,18 +1,28 @@
 package pocketclient
 
-import "errors"
-
 const (
-	EndpointAdmins                = "/api/admins"
-	EndpointAdminAuthWithPassword = EndpointAdmins + "/auth-with-password"
+	EndpointAdmins                     = "/api/admins"
+	EndpointAdminsAuthWithPassword     = EndpointAdmins + "/auth-with-password"
+	EndpointAdminsRequestPasswordReset = EndpointAdmins + "/request-password-reset"
+	EndpointAdminsConfirmPasswordReset = EndpointAdmins + "/confirm-password-reset"
+
+	EndpointHealthCheck = "/api/health"
+
+	EndpointLogs      = "/api/logs"
+	EndpointLogsStats = EndpointLogs + "/stats"
 )
 
 var (
 	HeaderAuthorizationToken = func() (string, string) {
-		return "Authorization", Client.PocketBase.FirstAdmin().Token
+		return "Authorization", Client.PocketBase.Credentials.Token
+	}
+
+	HeaderAuthorizationTokenFrom = func(c *PocketClient) (string, string) {
+		return "Authorization", c.PocketBase.Credentials.Token
 	}
 )
 
 var (
-	ErrUnableToAuthorizeFirstAdmin = errors.New("unable to authorize first admin")
+	ErrNotAuthenticated            = Error("pocketclient not authenticated (call [AdminModule] Admin.Login() to authenticate first admin)")
+	ErrUnableToAuthorizeFirstAdmin = Error("unable to authorize first admin")
 )
