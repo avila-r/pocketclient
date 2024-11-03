92b601c0c6f8a51c47727f603bc63a19baa371db
r. ávila <avila.dev@outlook.com>
2024-11-03 12:24:48 -0300

    fix nil deferences

--- collections/builder.go
+++ collections/builder.go
@@ -1,6 +1,8 @@
 package collections
 
-import "errors"
+import (
+	"errors"
+)
 
 type CollectionBuilder struct {
 	name    *string
@@ -61,16 +63,16 @@ func (b *CollectionBuilder) Build() (*CollectionRequest, error) {
 func (b *CollectionBuilder) defaults() {
 	// Must be base (default), auth or view.
 	if b.typ == nil || *b.typ == "" {
-		*b.typ = TypeBase
+		b.typ = ptr(TypeBase)
 	}
 
 	if b.sys == nil {
-		*b.sys = false
+		b.sys = ptr(false)
 	}
 }
 
 func (b *CollectionBuilder) validate() error {
-	if *b.typ == TypeBase && (b.schema == nil || len(*b.schema) <= 0) {
+	if b.typ == ptr(TypeBase) && (b.schema == nil || len(*b.schema) <= 0) {
 		return errors.New("schema is required for base collections")
 	}
 
@@ -78,24 +80,24 @@ func (b *CollectionBuilder) validate() error {
 		return errors.New("system must be true or false")
 	}
 
-	if r := b.rules.CreateRule; *b.typ == TypeView && (*r != "" || r != nil) {
-		return errors.New("create rule must be null for view collections")
-	}
-
-	if r := b.rules.UpdateRule; *b.typ == TypeView && (*r != "" || r != nil) {
-		return errors.New("update rule must be null for view collections")
-	}
-
-	if r := b.rules.DeleteRule; *b.typ == TypeView && (*r != "" || r != nil) {
-		return errors.New("delete rule must be null for view collections")
-	}
-
-	if i := b.indexes; *b.typ == TypeView && (len(*i) > 0 || i != nil) {
+	if i := b.indexes; b.typ == ptr(TypeView) && i != nil && len(*i) > 0 {
 		return errors.New("view collections don't support indexes")
 	}
 
-	if q := b.options.Query; *b.typ == TypeView && (q == nil || *q == "") {
+	if b.options != nil && b.typ == ptr(TypeView) && (b.options.Query == nil || b.options.Query == ptr("")) {
 		return errors.New("query is required for view collections")
+	}
+
+	if b.rules != nil && b.typ == ptr(TypeView) && b.rules.CreateRule != nil && b.rules.CreateRule != ptr(Rule("")) {
+		return errors.New("create rule must be null for view collections")
+	}
+
+	if b.rules != nil && b.typ == ptr(TypeView) && b.rules.UpdateRule != nil && b.rules.UpdateRule != ptr(Rule("")) {
+		return errors.New("update rule must be null for view collections")
+	}
+
+	if b.rules != nil && b.typ == ptr(TypeView) && b.rules.DeleteRule != nil && b.rules.DeleteRule != ptr(Rule("")) {
+		return errors.New("delete rule must be null for view collections")
 	}
 
 	return nil
@@ -105,30 +107,34 @@ func (b *CollectionBuilder) build() *CollectionRequest {
 	fields := []CollectionRequestField{}
 	for _, field := range *b.schema {
 		options := CollectionRequestFieldOptions{
-			Min:       field.O.Min,
-			Max:       field.O.Max,
-			Pattern:   field.O.Pattern,
-			MaxSelect: field.O.MaxSelect,
-			MaxSize:   field.O.MaxSize,
-			MimeTypes: field.O.MimeTypes,
-			Thumbs:    field.O.Thumbs,
+			Min:       field.Options.Min,
+			Max:       field.Options.Max,
+			Pattern:   field.Options.Pattern,
+			MaxSelect: field.Options.MaxSelect,
+			MaxSize:   field.Options.MaxSize,
+			MimeTypes: field.Options.MimeTypes,
+			Thumbs:    field.Options.Thumbs,
 		}
 
 		fields = append(fields, CollectionRequestField{
-			Name:     field.N,
-			Type:     field.T,
-			Required: field.R,
-			Unique:   field.U,
+			Name:     field.Name,
+			Type:     field.Type,
+			Required: field.Required,
+			Unique:   field.Unique,
 			Options:  options,
 		})
 	}
 
-	options := &CollectionRequestOptions{
-		ViewOptions: &ViewOptions{
-			Query: *b.options.Query,
-		},
+	var view *ViewOptions = &ViewOptions{}
+	if b.options != nil && b.options.Query != nil {
+		view.Query = *b.options.Query
+	} else {
+		view = nil
+	}
 
-		AuthOptions: &AuthOptions{
+	var auth *AuthOptions
+	if b.options != nil {
+		auth = &AuthOptions{
 			ManageRule:         b.options.ManageRule,
 			AllowOAuth2Auth:    b.options.AllowOAuth2Auth,
 			AllowUsernameAuth:  b.options.AllowUsernameAuth,
@@ -138,7 +144,33 @@ func (b *CollectionBuilder) build() *CollectionRequest {
 			OnlyEmailDomains:   b.options.OnlyEmailDomains,
 			OnlyVerified:       b.options.OnlyVerified,
 			MinPasswordLength:  b.options.MinPasswordLength,
-		},
+		}
+	}
+
+	options := &CollectionRequestOptions{
+		ViewOptions: view,
+		AuthOptions: auth,
+	}
+
+	var (
+		listRule   *Rule
+		viewRule   *Rule
+		createRule *Rule
+		updateRule *Rule
+		deleteRule *Rule
+	)
+
+	if b.rules != nil {
+		listRule = b.rules.ListRule
+		viewRule = b.rules.ViewRule
+		createRule = b.rules.CreateRule
+		updateRule = b.rules.UpdateRule
+		deleteRule = b.rules.DeleteRule
+	}
+
+	indexes := Indexes{}
+	if b.indexes != nil {
+		indexes = *b.indexes
 	}
 
 	request := CollectionRequest{
@@ -146,12 +178,12 @@ func (b *CollectionBuilder) build() *CollectionRequest {
 		Type:       *b.typ,
 		System:     b.sys,
 		Schema:     fields,
-		ListRule:   b.rules.ListRule,
-		ViewRule:   b.rules.ViewRule,
-		CreateRule: b.rules.CreateRule,
-		UpdateRule: b.rules.UpdateRule,
-		DeleteRule: b.rules.DeleteRule,
-		Indexes:    *b.indexes,
+		ListRule:   listRule,
+		ViewRule:   viewRule,
+		CreateRule: createRule,
+		UpdateRule: updateRule,
+		DeleteRule: deleteRule,
+		Indexes:    indexes,
 		Options:    *options,
 	}
 
@@ -162,20 +194,20 @@ func (b *CollectionBuilder) BuildPatch() *CollectionPatch {
 	fields := []CollectionPatchField{}
 	for _, field := range *b.schema {
 		options := CollectionPatchFieldOptions{
-			Min:       field.O.Min,
-			Max:       field.O.Max,
-			Pattern:   field.O.Pattern,
-			MaxSelect: field.O.MaxSelect,
-			MaxSize:   field.O.MaxSize,
-			MimeTypes: field.O.MimeTypes,
-			Thumbs:    field.O.Thumbs,
+			Min:       field.Options.Min,
+			Max:       field.Options.Max,
+			Pattern:   field.Options.Pattern,
+			MaxSelect: field.Options.MaxSelect,
+			MaxSize:   field.Options.MaxSize,
+			MimeTypes: field.Options.MimeTypes,
+			Thumbs:    field.Options.Thumbs,
 		}
 
 		fields = append(fields, CollectionPatchField{
-			Name:     &field.N,
-			Type:     &field.T,
-			Required: &field.R,
-			Unique:   &field.U,
+			Name:     &field.Name,
+			Type:     &field.Type,
+			Required: &field.Required,
+			Unique:   &field.Unique,
 			Options:  &options,
 		})
 	}
@@ -209,4 +241,8 @@ func (b *CollectionBuilder) BuildPatch() *CollectionPatch {
 	}
 
 	return &patch
+}
+
+func ptr[T any](v T) *T {
+	return &v
 }
